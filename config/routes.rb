bb47e931e043f3c158cfe0981febf7999be2d270
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-09-26 15:52:21 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get("/dice/2/6", { :controller => "rolldice", :action => "rock" })
+
+  get("/dice/2/10", { :controller => "rolldice", :action => "paper" })
+
+  get("/dice/1/20", { :controller => "rolldice", :action => "scissors" })
+
+  get("/dice/5/4", { :controller => "rolldice", :action => "scissors" })
+
+  get("/", { :controller => "rolldice", :action => "home"})
 
 end
